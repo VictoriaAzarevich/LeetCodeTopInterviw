e4f6832aa250a0300a1bfbb131be7cba2cecba72
Victoria Azarevich <vmisyakova@gmail.com>
2025-04-08 22:42:34 +0300

    Complete Rotate Array faster solution

--- RotateArray.cs
+++ RotateArray.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LeetCodeTopInterviw
+﻿namespace LeetCodeTopInterviw
 {
     public class RotateArray
     {
@@ -21,6 +15,29 @@ namespace LeetCodeTopInterviw
                     nums[j + 1] = previous;
                 }
                 nums[0] = next;
+            }
+        }
+
+        public static void RotateArrayMedium2(int[] nums, int k)
+        {
+            if (k == 0 || nums.Length == 1 || nums.Length == k)
+                return;
+
+            int s = 0;
+            int p = 0;
+            int e = nums[p];
+
+            for (int c = 1; c <= nums.Length; c++)
+            {
+                p = (p + k) % nums.Length;
+                (e, nums[p]) = (nums[p], e);
+
+                if (p == s)
+                {
+                    s++;
+                    p++;
+                    e = nums[p];
+                }
             }
         }
     }

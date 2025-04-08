c8ec6a04d1bd34a704814f2cbad203fe9a57cea8
Victoria Azarevich <vmisyakova@gmail.com>
2025-04-08 22:40:40 +0300

    Complete RotateArray (my solution)

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LeetCodeTopInterviw;
+
+RotateArray.RotateArrayMedium1(new int[] { 1, 2, 3, 4, 5, 6, 7 }, 3);
+
---
+++ RotateArray.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTopInterviw
+{
+    public class RotateArray
+    {
+        static public void RotateArrayMedium1(int[] nums, int k)
+        {
+            for (int i = 1; i <= k; i++)
+            {
+                int next = nums[0];
+                int previous = 0;
+                for (int j = 0; j < nums.Length - 1; j++)
+                {
+                    previous = next;
+                    next = nums[j + 1];
+                    nums[j + 1] = previous;
+                }
+                nums[0] = next;
+            }
+        }
+    }
+}

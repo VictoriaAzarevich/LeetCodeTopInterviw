6819c3691f45e6f744c359cfb9c42491d6c78ea8
Victoria Azarevich <vmisyakova@gmail.com>
2025-04-09 12:46:33 +0300

    Complete Best time to buy and sell stock

---
+++ BestTimeToBuyAndSellStock.cs
@@ -0,0 +1,56 @@
+﻿namespace LeetCodeTopInterviw
+{
+    public class BestTimeToBuyAndSellStock
+    {
+        public static int MaxProfit(int[] prices)
+        {
+            if (prices.Length == 1)
+            {
+                return 0;
+            }
+
+            int min = prices[0];
+            int max = prices[0];
+            int profit = 0;
+
+            for (int i = 1; i < prices.Length; i++)
+            {
+                if (min > prices[i])
+                {
+                    min = prices[i];
+                    max = prices[i];
+                }
+                else if (max < prices[i])
+                {
+                    max = prices[i];
+                    if (profit < max - min)
+                    {
+                        profit = max - min;
+                    }
+                }
+            }
+
+            return profit;
+        }
+
+        public static int MaxProfit1(int[] prices)
+        {
+            int max = 0;
+            int min = prices[0];
+
+            for (int i = 1; i < prices.Length; i++)
+            {
+                if (prices[i] < min)
+                {
+                    min = prices[i];
+                }
+
+                else if ((prices[i] - min) > max)
+                {
+                    max = prices[i] - min;
+                }
+            }
+            return max;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using LeetCodeTopInterviw;
 
-RotateArray.RotateArrayMedium1(new int[] { 1, 2, 3, 4, 5, 6, 7 }, 3);
+// RotateArray.RotateArrayMedium1(new int[] { 1, 2, 3, 4, 5, 6, 7 }, 3);
+BestTimeToBuyAndSellStock.MaxProfit1(new int[] { 7, 1, 5, 3, 6, 4 });
 

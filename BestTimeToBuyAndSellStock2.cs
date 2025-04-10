0609c936a4d3d225742e37bbc07bca2986611023
Victoria Azarevich <vmisyakova@gmail.com>
2025-04-10 14:35:29 +0300

    Complete BestTimeToBuyAndSellStock II

---
+++ BestTimeToBuyAndSellStock2.cs
@@ -0,0 +1,53 @@
+﻿namespace LeetCodeTopInterviw
+{
+    public class BestTimeToBuyAndSellStock2
+    {
+        public static int MaxProfit(int[] prices)
+        {
+            int max = 0;
+            int min = prices[0];
+            int profit = 0;
+
+            for (int i = 1; i < prices.Length; i++)
+            {
+                if (min > prices[i])
+                {
+                    min = prices[i];
+                }
+                else if (prices[i] - min >= max)
+                {
+                    max = prices[i] - min;
+                    if (i >= prices.Length - 1)
+                    {
+                        profit += max;
+                    }
+                    else if (prices[i + 1] < prices[i])
+                    {
+                        profit += max;
+                        max = 0;
+                        min = prices[i + 1];
+                        i++;
+                    }
+                }
+            }
+
+            return profit;
+        }
+
+        public static int MaxProfit1(int[] prices)
+        {
+            int max = 0;
+            int start = prices[0];
+            int len = prices.Length;
+            for (int i = 1; i < len; i++)
+            {
+                if (start < prices[i])
+                {
+                    max += prices[i] - start;
+                }
+                start = prices[i];
+            }
+            return max;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿using LeetCodeTopInterviw;
 
 // RotateArray.RotateArrayMedium1(new int[] { 1, 2, 3, 4, 5, 6, 7 }, 3);
-BestTimeToBuyAndSellStock.MaxProfit1(new int[] { 7, 1, 5, 3, 6, 4 });
-
+// BestTimeToBuyAndSellStock.MaxProfit1(new int[] { 7, 1, 5, 3, 6, 4 });
+BestTimeToBuyAndSellStock2.MaxProfit1(new int[] { 5, 2, 3, 2, 6, 6, 2, 9, 1, 0, 7, 4, 5, 0 });

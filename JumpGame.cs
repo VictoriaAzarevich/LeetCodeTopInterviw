b35e2f28ac05b96308d3baf80f7a422145fde9eb
Victoria Azarevich <vmisyakova@gmail.com>
2025-04-11 22:01:05 +0300

    Complete my solution of Jump Game

---
+++ JumpGame.cs
@@ -0,0 +1,49 @@
+﻿namespace LeetCodeTopInterviw
+{
+    public class JumpGame
+    {
+        public static bool CanJump(int[] nums)
+        {
+            if (nums.Length == 1)
+            {
+                return true;
+            }
+
+            if (nums[0] == 0)
+            {
+                return false;
+            }
+
+            int indexOfZero = 0;
+            int i = 0;
+
+            while (true)
+            {
+                if (nums[i] + i >= nums.Length - 1)
+                {
+                    return true;
+                }
+
+                if (indexOfZero >= i + nums[i])
+                {
+                    i--;
+                    if (i < 0)
+                    {
+                        return false;
+                    }
+                    continue;
+                }
+
+                if (nums[i] == 0)
+                {
+                    indexOfZero = i;
+                    i--;
+                    continue;
+                }
+
+                indexOfZero = 0;
+                i += nums[i];
+            }
+        }
+    }
+}

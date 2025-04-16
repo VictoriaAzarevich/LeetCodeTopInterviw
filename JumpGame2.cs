c9b0e30028d605a9567800d069803e88d3b0faa9
Victoria Azarevich <vmisyakova@gmail.com>
2025-04-16 12:16:01 +0300

    Complete Jump Game 2

---
+++ JumpGame2.cs
@@ -0,0 +1,66 @@
+﻿
+namespace LeetCodeTopInterviw
+{
+    public class JumpGame2
+    {
+        public static int CanJump(int[] nums)
+        {
+            if (nums.Length == 1)
+            {
+                return 0;
+            }
+
+            int step = 1;
+            int curElIndex = 0;
+            int max = 0;
+            int maxIndex = 0;
+            if (nums[0] >= nums.Length - 1)
+            {
+                return step;
+            }
+            for (int i = 1; i <= nums.Length - 2; i++)
+            {
+                if (i > curElIndex + nums[curElIndex])
+                {
+                    step++;
+                    curElIndex = maxIndex;
+                    i = maxIndex;
+                    max = 0;
+                    maxIndex = 0;
+                }
+                else if (i - curElIndex + nums[i] >= max)
+                {
+                    max = i - curElIndex + nums[i];
+                    maxIndex = i;
+                }
+
+                if (i + nums[i] >= nums.Length - 1)
+                {
+                    return ++step;
+                }
+            }
+
+            return step;
+        }
+
+        public static int CanJump1(int[] nums)
+        {
+            int jumps = 0, farthest = 0, end = 0;
+
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                // Update the farthest point we can reach
+                farthest = Math.Max(farthest, i + nums[i]);
+
+                // If we've reached the current end, we need to jump
+                if (i == end)
+                {
+                    jumps++;
+                    end = farthest; // Update the range for the next jump
+                }
+            }
+
+            return jumps;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using LeetCodeTopInterviw;
+using System.Globalization;
 
 // RotateArray.RotateArrayMedium1(new int[] { 1, 2, 3, 4, 5, 6, 7 }, 3);
 // BestTimeToBuyAndSellStock.MaxProfit1(new int[] { 7, 1, 5, 3, 6, 4 });
 //BestTimeToBuyAndSellStock2.MaxProfit1(new int[] { 5, 2, 3, 2, 6, 6, 2, 9, 1, 0, 7, 4, 5, 0 });
-Console.WriteLine(JumpGame.CanJump([ 2, 0, 1, 0, 1]));
+Console.WriteLine(JumpGame2.CanJump1([2, 3, 1, 1, 4]));
+

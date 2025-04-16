a7fdf464cff4e25cb5dc207b486d54ff928efdc9
Victoria Azarevich <vmisyakova@gmail.com>
2025-04-16 16:01:40 +0300

    Solve HIndex

---
+++ HIndex.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTopInterviw
+{
+    public class HIndex
+    {
+        public static int HIndexSolution(int[] citations)
+        {
+            int hIndex = 0;
+            for (int i = 1; i <= citations.Length; i++)
+            {
+                int count = 0;
+                for (int j = 0; j < citations.Length; j++)
+                {
+                    if (citations[j] >= i)
+                    {
+                        count++;
+                    }
+
+                    if (count == i)
+                    {
+                        hIndex = i;
+                        break;
+                    }
+                }
+
+                if (count == 0)
+                {
+                    return hIndex;
+                }
+            }
+
+            return hIndex;
+        }
+    }
+}

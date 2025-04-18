ee1930d0eaf41d8a80c0e98e07079448259e2cd7
Victoria Azarevich <vmisyakova@gmail.com>
2025-04-18 12:00:03 +0300

    Faster solution

--- RandomizedSet.cs
+++ RandomizedSet.cs
@@ -40,5 +40,52 @@
         {
             return set[rnd.Next(0, set.Count)];
         }
+
+        /* private List<int> nums;
+        private Dictionary<int, int> indexMap;
+        private Random rand;
+        public RandomizedSet()
+        {
+            nums = new List<int>();
+            indexMap = new Dictionary<int, int>();
+            rand = new Random();
+        }
+
+        public bool Insert(int val)
+        {
+            if (indexMap.ContainsKey(val))
+            {
+                return false;
+            }
+
+            nums.Add(val);
+            indexMap[val] = nums.Count - 1;
+            return true;
+        }
+
+        public bool Remove(int val)
+        {
+            if (!indexMap.ContainsKey(val))
+            {
+                return false;
+            }
+
+            int lastElement = nums[nums.Count - 1];
+            int valIndex = indexMap[val];
+
+            nums[valIndex] = lastElement;
+            indexMap[lastElement] = valIndex;
+
+            nums.RemoveAt(nums.Count - 1);
+            indexMap.Remove(val);
+
+            return true;
+        }
+
+        public int GetRandom()
+        {
+            int randomIndex = rand.Next(nums.Count);
+            return nums[randomIndex];
+        } */
     }
 }

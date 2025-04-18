aaab1e7a6de8e8176891f895d7e001bb185c9d44
Victoria Azarevich <vmisyakova@gmail.com>
2025-04-18 11:57:29 +0300

    Solve Insert Delete GetRandom O(1)

---
+++ RandomizedSet.cs
@@ -0,0 +1,44 @@
+﻿namespace LeetCodeTopInterviw
+{
+    public class RandomizedSet
+    {
+        private List<int> set;
+        private Random rnd;
+        public RandomizedSet()
+        {
+            set = new List<int>();
+            rnd = new Random();
+        }
+
+        public bool Insert(int val)
+        {
+            if (!set.Contains(val))
+            {
+                set.Add(val);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool Remove(int val)
+        {
+            if (set.Contains(val))
+            {
+                set.Remove(val);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public int GetRandom()
+        {
+            return set[rnd.Next(0, set.Count)];
+        }
+    }
+}
